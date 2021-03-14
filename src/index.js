d50a25056ec08890801dcf6bc6a740cd21131500
FutureMask <maks564698@gmail.com>
2021-03-14 23:17:44 +0300

    init: task solutione

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+    var ones = new Array('', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen');
+    var tens = new Array('', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety');
+    var hundred = ' hundred ';
+    var output = '';
+    var numString = num.toString();
+
+    if (num == 0) {
+        return 'zero';
+    }
+    // 100 and more
+if ( numString.length == 3 ) 
+{
+  output  = ones[ parseInt( numString.charAt(0) ) ] + hundred ;
+  num = num % 100 ;
+  numString = num.toString() ;
 }
+
+if ( num < 20 )  
+{
+  output += ones[num] ;
+}
+else 
+{ // 20-99 
+  output += '' + tens[ parseInt( numString.charAt(0) ) ] + ' ';
+  output += ones[ parseInt( numString.charAt(1) ) ] ; 
+}
+
+return output;
+    }
